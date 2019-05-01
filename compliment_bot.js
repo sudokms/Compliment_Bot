6d23ea1f3de5fb649aeccea26dd8a5419c7470a0
Riley <50162982+sudokms@users.noreply.github.com>
2019-05-01 09:41:43 -0400

    Update compliment_bot.js

--- compliment_bot.js
+++ compliment_bot.js
@@ -1,1 +1,9 @@
-
+var response
+var person = prompt("What's your name?")
+switch(person) 
+  case "Alexis"
+  case "alexis"
+    response = "Oh! Hi, Sweetie!"
+    break;
+  default:
+   response = "Hi, " + person + "!"
